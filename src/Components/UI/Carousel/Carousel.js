34e514d03190a16d5b29aa1de6b12457b810b357
NicolasDenis57 <nicolas.denis.57@gmail.com>
2023-06-10 10:54:28 +0200

    carousel logic done, need to change position comportment

--- src/Components/UI/Carousel/Carousel.js
+++ src/Components/UI/Carousel/Carousel.js
@@ -1,46 +1,74 @@
 import React, { useState, useEffect } from 'react';
 import styles from './Carousel.module.css';
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 
 const Carousel = ({ images }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [activeSlideIndex, setActiveSlideIndex] = useState(0);
   const [isHovered, setIsHovered] = useState(false);
 
   const nextSlide = () => {
     const newIndex = (currentIndex + 1) % images.length;
     setCurrentIndex(newIndex);
+    setActiveSlideIndex(newIndex);
   };
 
   const prevSlide = () => {
     const newIndex = (currentIndex - 1 + images.length) % images.length;
     setCurrentIndex(newIndex);
+    setActiveSlideIndex(newIndex);
+  };
+
+  const handleIndicatorClick = (index) => {
+    setCurrentIndex(index);
+    setActiveSlideIndex(index);
   };
 
   useEffect(() => {
-      let interval = null;
+    let interval = null;
 
-      if (!isHovered) {
-            interval = setInterval(() => {
-              nextSlide();
-            }, 2000);
-          }
-  
-      return () => clearInterval(interval);
-    }, [currentIndex, isHovered]);
+    if (!isHovered) {
+      interval = setInterval(nextSlide, 2000);
+    }
+
+    return () => clearInterval(interval);
+  }, [currentIndex, isHovered]);
 
   return (
-    <div className={styles.carousel} 
-    onMouseEnter={() => setIsHovered(true)}
+    <div
+      className={styles.carousel}
+      onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
-      >
+    >
       <div className={styles['carousel-image-container']}>
-        <img src={images[currentIndex]} alt="carousel-slide" />
-        <button className={`${styles['carousel-button']} ${styles['prev-button']}`} onClick={prevSlide}>
-          Previous
-        </button>
-        <button className={`${styles['carousel-button']} ${styles['next-button']}`} onClick={nextSlide}>
-          Next
-        </button>
+        {images.map((image, index) => (
+          <img
+            key={index}
+            src={image}
+            alt="carousel-slide"
+            className={`${styles['carousel-image']} ${
+              index === currentIndex ? styles['fade-in'] : styles['fade-out']
+            }`}
+          />
+        ))}
+        <div className={styles['carousel-indicators']}>
+          {images.map((_, index) => (
+            <div
+              key={index}
+              className={`${styles['carousel-indicator']} ${
+                index === activeSlideIndex ? styles['active'] : ''
+              }`}
+              onClick={() => handleIndicatorClick(index)}
+            />
+          ))}
+        </div>
       </div>
+      <button className={`${styles['carousel-button']} ${styles['prev-button']}`} onClick={prevSlide}>
+        <FaChevronLeft />
+      </button>
+      <button className={`${styles['carousel-button']} ${styles['next-button']}`} onClick={nextSlide}>
+        <FaChevronRight />
+      </button>
     </div>
   );
 };
